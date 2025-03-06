349bcae70d9278889bab13150354dbeea9d85aef
Mohamed ElHelaly <138399707+NetNinjaEngineer@users.noreply.github.com>
2025-03-06 15:26:15 +0200

    Add project files.

---
+++ TangyWeb_Server/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TangyWeb_Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<Components.App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
---
+++ Tangy_Models/LearnBlazorModels/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Tangy_Models.LearnBlazorModels;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public decimal Price { get; set; }
+}
